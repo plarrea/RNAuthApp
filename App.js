5f7dd97f96a77a719169721a4a318aeb48f94fc3
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-10 17:33:56 -0500

    implement auto login with stored token

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import IconButton from './src/components/ui/IconButton';
 import { Colors } from './src/constants/styles';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
+import AuthContextProvider from './src/store/auth-context';
+import useAuth from './src/store/use-auth';
 
+SplashScreen.preventAutoHideAsync();
 const Stack = createNativeStackNavigator();
 
 function AuthStack() {
@@ -25,6 +32,7 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const { logout } = useAuth();
   return (
     <Stack.Navigator
       screenOptions={{
@@ -33,24 +41,72 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              color={tintColor}
+              size={24}
+              onPress={logout}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
 
 function Navigation() {
+  const { isAuthenticated } = useAuth();
   return (
     <NavigationContainer>
-      <AuthStack />
+      {isAuthenticated ? <AuthenticatedStack /> : <AuthStack />}
     </NavigationContainer>
   );
+}
+
+function Root() {
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+
+  const { authenticate } = useAuth();
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token');
+
+      if (storedToken) {
+        authenticate(storedToken);
+      }
+
+      setIsTryingLogin(false);
+    }
+
+    fetchToken();
+  }, []);
+
+  useEffect(() => {
+    if (!isTryingLogin) {
+      SplashScreen.hide();
+    }
+  }, [isTryingLogin]);
+
+  if (isTryingLogin) {
+    return null;
+  }
+
+  return <Navigation />;
 }
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <Navigation />
+      <AuthContextProvider>
+        <Root />
+      </AuthContextProvider>
     </>
   );
 }
